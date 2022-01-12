9761f2e4fc7679cbd1c3d1ce4e6416d20f1dd1cc
Ashish Rajesh Lulla <65806448+Ashishlulla@users.noreply.github.com>
2022-01-12 20:59:26 +0530

    Create Practice Problem 4.py

---
+++ Practice Problems - Level 1/Practice Problem 4.py
@@ -0,0 +1,34 @@
+#  Problem Statement
+
+"""
+Given a list of numbers, write a python function which returns true if one of the first 4 elements in the list is 9. Otherwise it should return false.
+
+The length of the list can be less than 4 also.
+
+Sample Input                          Expected Output
+
+[1, 2, 9, 3, 4]                          True
+ 
+[1, 2, 9]                                True
+
+[1, 2,3,4]                               False
+
+"""
+
+# Code
+
+def find_nine(nums):
+
+    if 9 in nums:
+        ind = nums.index(9)
+        if 0 <= ind < 4:
+            return True
+        else:
+            return False
+    else:
+        return False
+
+
+nums = [1, 9, 4, 5, 6]
+print(find_nine(nums))
+
